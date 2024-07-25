a7552ba8e9481ddadc9f9dceaa6fcab2d46fc49b
Mostafa_Asaad <ma4190243@gmail.com>
2024-07-25 22:43:33 +0300

    SysTick Timer Configurations file

---
+++ Inc/CortexM4/SysTick/SysTick_Cfg.h
@@ -0,0 +1,16 @@
+/**
+ ******************************************************************************
+ * @file           : SysTick_Cfg.h
+ * @author         : Mostafa Asaad (https://github.com/M0stafa077)
+ * @brief          : System-Timer Device-Driver Configurations File.
+ ******************************************************************************
+ */
+#ifndef CORTEXM4_SYSTICK_SYSTICK_CFG_H_
+#define CORTEXM4_SYSTICK_SYSTICK_CFG_H_
+
+#define SYSTICK_PROCESSOR_CLOCK		0UL
+#define SYSTICK_EXTERNAL_CLOCK		1UL
+
+#define SYSTICK_CLOCK_SOURCE		SYSTICK_EXTERNAL_CLOCK
+
+#endif /* CORTEXM4_SYSTICK_SYSTICK_CFG_H_ */
