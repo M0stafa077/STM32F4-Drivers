56ac918730206bca1c58b7125f1936c30b423d0f
Mostafa_Asaad <ma4190243@gmail.com>
2024-07-25 22:46:39 +0300

    Testing Systick Timer

--- Src/main.c
+++ Src/main.c
@@ -10,21 +10,33 @@
 #include "CortexM4/NVIC/NVIC.h"
 #include "Common/stm32f401_registers.h"
 #include "MCAL/RCC/rcc.h"
-
+#include "CortexM4/SysTick/SysTick.h"
 
 void RCC_Clock_Config();
 
-int x = 0;
+
+static volatile int x = 0;
+Std_ReturnType_t retVal = E_OK;
+
+void SysTick_ExcepHandler(void)
+{
+	x++;
+}
 
 int main(void)
 {
-
+	NVIC_SetPriorityGrouping(NVIC_PRIORITY_GROUP_2_BITS);
 	RCC_Clock_Config();
+	uint32_t No_Of_Ticks = 2000000;
+	retVal = SysTick_Init(No_Of_Ticks);
+
+	retVal = SysTick_PeriodicInterval(No_Of_Ticks, SysTick_ExcepHandler);
+
     while(1)
     {
-    	x++;
-    	if(100 == x) x = 0;
+
     }
+    return 0;
 }
 
 void RCC_Clock_Config()
@@ -32,18 +44,17 @@ void RCC_Clock_Config()
 	RCC_InitConfigs_t rcc_cfgs =
 	{
 		.Oscillator_Configurations = { RCC_HSE_OFF, RCC_HSI_ON, RCC_PLLI2S_OFF },
-		.Clock_Source = RCC_CLOCK_SOURCE_PLL,
-		.PLL_Configurations = {
-				RCC_PLL_SOURE_HSI,		// Clock Src
-				168,					// N
-				8,						// M
-				4,						// Q
-				RCC_PLL_P_DIVIDE_BY_4	// P
-
-		},
+		.Clock_Source = RCC_CLOCK_SOURCE_HSI,
 		.AHB_Prescaler = SYSTEM_CLOCK_NOT_DIVIDED,
-		.APB1_Prescaler = APB1_CLOCK_DIVIDED_BY_2,
-		.APB2_Prescaler = APB2_CLOCK_NOT_DIVIDED
+		.APB1_Prescaler = APB1_CLOCK_NOT_DIVIDED,
+		.APB2_Prescaler = APB2_CLOCK_NOT_DIVIDED,
+		.PLL_Configurations = {
+			RCC_PLL_SOURE_HSI,		// Clock Src
+			0xA8,					// N
+			0x08,					// M
+			0x04,					// Q
+			RCC_PLL_P_DIVIDE_BY_4	// P
+		}
 	};
 	RCC_Init(&rcc_cfgs);
 }
